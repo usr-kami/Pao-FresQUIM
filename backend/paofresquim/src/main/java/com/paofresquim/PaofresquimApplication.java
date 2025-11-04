475d03a18a38a322582aa44d3d1309e287fb1383
koi <koi@email.com>
2025-11-04 00:19:40 -0300

    Commit de configurações iniciais do projeto Pão-FresQUIM

---
+++ backend/paofresquim/src/main/java/com/paofresquim/PaofresquimApplication.java
@@ -0,0 +1,13 @@
+package com.paofresquim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaofresquimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaofresquimApplication.class, args);
+	}
+
+}
---
+++ backend/paofresquim/src/test/java/com/paofresquim/PaofresquimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paofresquim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaofresquimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
