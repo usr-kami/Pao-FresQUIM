caa896bdc4e4267b3c8b16dca56aa39da471f469
koi <koi@email.com>
2025-11-04 22:27:44 -0300

    db: executa todos os scripts de inicialização

---
+++ backend/paofresquim/database/migrations/initial_schema.sql
@@ -0,0 +1,7 @@
+.read schemas/clientes.sql
+.read schemas/produtos.sql
+.read schemas/estoque_ingredientes.sql
+.read schemas/vendas.sql
+.read constraints/fk_vendas_clientes.sql
+.read constraints/fk_vendas_produtos.sql
+.read triggers/tg_processar_venda.sql
